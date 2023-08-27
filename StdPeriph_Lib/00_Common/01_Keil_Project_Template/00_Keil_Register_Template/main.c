05b8a52d5e772783c897d3b235653f79a3452962
Chong Liu <liuc12@miamioh.edu>
2023-08-27 08:46:32 +0800

    Update main.c

--- StdPeriph_Lib/00_Common/01_Keil_Project_Template/00_Keil_Register_Template/main.c
+++ StdPeriph_Lib/00_Common/01_Keil_Project_Template/00_Keil_Register_Template/main.c
@@ -6,6 +6,7 @@ void SystemInit(void) {
 
 int main(void) {
     /* RCC_APB2ENR 打开 GPIOB 端口的时钟 */
+    /* IOPBEN：设置为 1 */
     *(unsigned int *)0x40021018 |= (1 << (1 * 3));  /* 设置为1，1位为一组，向左移动3位 */
 
     /* GPIOx_CRL 配置 PB0 口为输出 */
@@ -13,6 +14,7 @@ int main(void) {
     *(unsigned int *)0x40010C00 |= (5 << (4 * 0));  /* 设置为1，4位为一组，向左移动0位 */
 
     /* GPIOx_ODR 控制 ODR 寄存器 */
+    /* ODR0：设置为 0 */
     *(unsigned int *)0x40010C0C &= ~(1 << (1 * 0)); /* 设置为0，1位为一组，向左移动0位 */
     return 0;
 }
