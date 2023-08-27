d77f5920c9cd897d6d22f9f82daae0d90708feff
Chong Liu <liuc12@miamioh.edu>
2023-08-27 16:37:54 +0800

    update

--- StdPeriph_Lib/00_Common/01_Keil_Project_Template/00_Keil_Register_Template/main.c
+++ StdPeriph_Lib/00_Common/01_Keil_Project_Template/00_Keil_Register_Template/main.c
@@ -1,28 +1,10 @@
 #include "stm32f10x.h"  /* 实现寄存器定义 */
 
-#define SECTION_ENABLE 1
-#define SECTION_DISABLE 0
-
 void SystemInit(void) {
     /* 函数为空，为了让编译器不报错 */
 }
 
 int main(void) {
-
-#if SECTION_EN
-    /* NOTE: */
-    /* RCC_APB2ENR 打开 GPIOB 端口的时钟 */
-    /* IOPBEN：设置为 1 */
-    *(unsigned int *)0x40021018 |= (1 << (1 * 3));  /* 设置为1，1位为一组，向左移动3位 */
-
-    /* GPIOx_CRL 配置 PB0 口为输出 */
-    /* 5 = 0101 - CNF0：01：通用开漏输出模式 - MODE0：01：输出模式，最大速度10MHz */
-    *(unsigned int *)0x40010C00 |= (5 << (4 * 0));  /* 设置为1，4位为一组，向左移动0位 */
-
-    /* GPIOx_ODR 控制 ODR 寄存器 */
-    /* ODR0：设置为 0 */
-    *(unsigned int *)0x40010C0C &= ~(1 << (1 * 0)); /* 设置为0，1位为一组，向左移动0位 */
-#endif
 
     return 0;
 }
