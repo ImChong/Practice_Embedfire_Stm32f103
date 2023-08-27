2f0c8fb241bc6b2212b97260ce56a653e1d4cf7f
Chong Liu <liuc12@miamioh.edu>
2023-08-27 16:56:04 +0800

    Update stm32f10x.h

--- StdPeriph_Lib/01_Registry/00_Keil_Register_Project/stm32f10x.h
+++ StdPeriph_Lib/01_Registry/00_Keil_Register_Project/stm32f10x.h
@@ -2,3 +2,7 @@
 
 /* 外设基地址 */
 #define PERIPH_BASE           ((uint32_t)0x40000000)    /* NOTE: Peripheral base address in the alias region */
+
+#define APB1PERIPH_BASE       PERIPH_BASE
+#define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
+#define AHBPERIPH_BASE        (PERIPH_BASE + 0x20000)
