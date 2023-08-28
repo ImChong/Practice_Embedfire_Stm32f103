1c48a204d13bf134ac580c6fd24d43bfe71b1e8a
Chong Liu <liuc12@miamioh.edu>
2023-08-29 00:36:36 +0800

    Update stm32f10x_gpio.c

--- 01_StdPeriph_Lib/01_Registry/00_Keil_Register_Project/stm32f10x_gpio.c
+++ 01_StdPeriph_Lib/01_Registry/00_Keil_Register_Project/stm32f10x_gpio.c
@@ -1,9 +1,25 @@
 #include "stm32f10x_gpio.h"
 
+/* GPIO_SetBits()
+ *  Functionality: 为GPIO引脚设置高电平
+ *  Arguments: GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin
+ *  Return: void
+ *  References:
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
 void GPIO_SetBits(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
     GPIOx->BSRR |= GPIO_Pin;
 }
 
+/* GPIO_ResetBits()
+ *  Functionality: 为GPIO引脚设置低电平
+ *  Arguments: GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin
+ *  Return: void
+ *  References:
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
 void GPIO_ResetBits(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
     GPIOx->BRR |= GPIO_Pin;
 }
