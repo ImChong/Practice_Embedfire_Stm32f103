c6880ca77c1ec24ed7c4655ab9988aa566d18454
Chong Liu <liuc12@miamioh.edu>
2023-08-29 00:24:06 +0800

    Update main.c

--- 01_StdPeriph_Lib/01_Registry/00_Keil_Register_Project/main.c
+++ 01_StdPeriph_Lib/01_Registry/00_Keil_Register_Project/main.c
@@ -3,8 +3,8 @@
 
 #define SECTION_ADDRESS_EN 0    /* 用地址直接操作 GPIO PB0 */
 #define SECTION_MACRO_EN 0      /* 用寄存器宏操作 GPIO PB0 */
-#define SECTION_STRUCT_EN 1     /* 用结构体操作 GPIO PB0 */
-#define SECTION_FUNCTION_EN 0   /* 用函数操作 GPIO PB0 */
+#define SECTION_STRUCT_EN 0     /* 用结构体操作 GPIO PB0 */
+#define SECTION_FUNCTION_EN 1   /* 用函数操作 GPIO PB0 */
 
 void SystemInit(void) {
     /* 函数为空，为了让编译器不报错 */
@@ -67,7 +67,20 @@ int main(void) {
 
 #elif SECTION_FUNCTION_EN
     /* NOTE: 用函数操作 GPIO PB0 */
+    /* RCC_APB2ENR 打开 GPIOB 端口的时钟 */
+    /* IOPBEN：设置为 1 */
+    RCC->APB2ENR |= (1 << (1 * 3));          /* 设置为1，1位为一组，向左移动3位 */
 
+    /* GPIOB_CRL 配置 PB0 清零 */
+    GPIOB->CRL &= ~(0x0f << (4 * 0));        /* 设置为0x0f，4位为一组，向左移动0位 */
+    /* GPIOx_CRL 配置 PB0 口为输出 */
+    /* 5 = 0101 - CNF0：01：通用开漏输出模式 - MODE0：01：输出模式，最大速度10MHz */
+    /* 1 = 0001 - CNF0：00：通用推挽输出模式 - MODE0：01：输出模式，最大速度10MHz */
+    GPIOB->CRL |= (1 << (4 * 0));            /* 设置为1，4位为一组，向左移动0位 */
+
+    /* GPIOx_ODR 控制 ODR 寄存器 */
+    /* ODR0：设置为 0 */
+    // GPIO_SetBits(GPIOB, GPIO_Pin_0);
 
 #endif
 
