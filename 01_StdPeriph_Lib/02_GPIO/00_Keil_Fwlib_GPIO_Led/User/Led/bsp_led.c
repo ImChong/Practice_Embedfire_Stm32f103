363af3b73822773119160fddf16a23cd831d7277
Chong Liu <liuc12@miamioh.edu>
2023-09-01 00:01:38 +0800

    update

--- 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.c
+++ 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.c
@@ -6,6 +6,18 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-08-31 11:48:57 pm
+ * Last Modified: Chong Liu - 2023-09-01 12:01:32 am
  */
 #include "bsp_led.h"
+
+/* LED_GPIO_Config()
+ *  Functionality: description...
+ *  Arguments: void
+ *  Return: void
+ *  References: LED GPIO 初始化
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
+void LED_GPIO_Config(void) {
+
+}
--- 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.h
+++ 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.h
@@ -6,13 +6,13 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-08-31 11:52:46 pm
+ * Last Modified: Chong Liu - 2023-09-01 12:00:50 am
  */
 #ifndef __BSD_LED_H
 #define __BSD_LED_H
 
 #include "stm32f10x.h"
 
-
+void LED_GPIO_Config(void);
 
 #endif  /* __BSD_LED_H */
