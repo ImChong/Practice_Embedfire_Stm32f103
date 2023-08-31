7cfa9616ffd6d96b67d5853fc26ad9db5a2b7ee7
Chong Liu <liuc12@miamioh.edu>
2023-08-31 23:36:48 +0800

    update

---
+++ 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/main.c
@@ -0,0 +1,24 @@
+/*
+ * @File: main.c
+ * @Created Date: 2023-08-31 5:17:04 pm
+ * @Author: Chong Liu
+ * @Brief: Description of what this file does
+ * =================================================================================
+ * Copyright (c) 2023 Chong Liu
+ * =================================================================================
+ * Last Modified: Chong Liu - 2023-08-31 5:45:22 pm
+ */
+#include "stm32f10x.h"
+
+/* main()
+ *  Functionality: description...
+ *  Arguments: void
+ *  Return: int
+ *  References: document/website...
+ ***********************************************************************************
+ *  IMPORTANT NOTICE FOR READER
+ */
+int main(void)
+{
+	// 来到这里的时候，系统的时钟已经被配置成72M。
+}
