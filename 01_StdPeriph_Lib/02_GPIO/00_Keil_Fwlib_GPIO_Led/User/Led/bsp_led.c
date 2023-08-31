40f68e91f63758804e8f95a99a0dc09469074041
Chong Liu <liuc12@miamioh.edu>
2023-08-31 23:47:22 +0800

    update

--- 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.c
+++ 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/Led/bsp_led.c
@@ -2,9 +2,9 @@
  * @File: bsp_led.c
  * @Created Date: 2023-08-31 11:38:57 pm
  * @Author: Chong Liu
- * @Brief: Description of what this file does
+ * @Brief: bsp - board support package 板级支持包
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-08-31 11:38:58 pm
+ * Last Modified: Chong Liu - 2023-08-31 11:46:32 pm
  */
