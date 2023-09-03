7e6833957850d7f9fc14b09b0c373e4917f35117
Chong Liu <liuc12@miamioh.edu>
2023-09-03 22:42:08 +0800

    Update bsp_key.c

--- 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/Button/bsp_key.c
+++ 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/Button/bsp_key.c
@@ -6,24 +6,24 @@
  * =================================================================================
  * Copyright (c) 2023 Chong Liu
  * =================================================================================
- * Last Modified: Chong Liu - 2023-09-03 9:57:39 pm
+ * Last Modified: Chong Liu - 2023-09-03 10:42:02 pm
  */
 #include "bsp_key.h"    /* 此头文件将添加 stm32f10x.h */
 
-/* functionName()
+/* KEY_GPIO_Config()
  *  Functionality: description...
- *  Arguments: arguments...
- *  Return: possibleReturnValues
+ *  Arguments: uint32_t GPIO_Clock, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin
+ *  Return: void
  *  References: document/website...
  ***********************************************************************************
  *  IMPORTANT NOTICE FOR READER
  */
-void KEY_GPIO_Config(void) {
+void KEY_GPIO_Config(uint32_t GPIO_Clock, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
     GPIO_InitTypeDef GPIO_InitStructure;                                /* GPIO 端口初始化结构体 */
-    RCC_APB2PeriphClockCmd(KEY1_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClockCmd(GPIO_Clock, ENABLE);
 
-    GPIO_InitStructure.GPIO_Pin = KEY1_PIN;                             /* 选择要控制的GPIO 引脚*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin;                             /* 选择要控制的GPIO 引脚*/
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;               /* 设置引脚的输出类型为浮空输入：硬件已外部下拉 */
     /* 输入无需设置速率 */
-    GPIO_Init(KEY1_GPIO, &GPIO_InitStructure);
+    GPIO_Init(GPIOx, &GPIO_InitStructure);
 }
