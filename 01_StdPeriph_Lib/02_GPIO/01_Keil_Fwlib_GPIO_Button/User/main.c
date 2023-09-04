9ec20dd0e59c3d77b9204cb867a9cf904ec94b67
Chong Liu <liuc12@miamioh.edu>
2023-09-04 23:14:41 +0800

    update

--- 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/main.c
+++ 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-02 17:29:59
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-04 23:04:28
+ * @LastEditTime : 2023-09-04 23:13:30
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -33,6 +33,49 @@
 #define MEM_ADDR(addr)          *((volatile unsigned long *)(addr))
 /* 把位带别名区地址转换成指针 */
 #define BIT_ADDR(addr, bitNum)  MEM_ADDR(BITBAND(addr, bitNum))
+
+/* ==================================================================================================== */
+/* ==================================================================================================== */
+/* GPIO ODR 和 IDR 寄存器地址映射 */
+#define GPIOA_ODR_Addr (GPIOA_BASE + 0x0C) //0x4001080C
+#define GPIOB_ODR_Addr (GPIOB_BASE + 0x0C) //0x40010C0C
+#define GPIOC_ODR_Addr (GPIOC_BASE + 0x0C) //0x4001100C
+#define GPIOD_ODR_Addr (GPIOD_BASE + 0x0C) //0x4001140C
+#define GPIOE_ODR_Addr (GPIOE_BASE + 0x0C) //0x4001180C
+#define GPIOF_ODR_Addr (GPIOF_BASE + 0x0C) //0x40011A0C
+#define GPIOG_ODR_Addr (GPIOG_BASE + 0x0C) //0x40011E0C
+
+#define GPIOA_IDR_Addr (GPIOA_BASE + 0x0C) //0x40010808
+#define GPIOB_IDR_Addr (GPIOB_BASE + 0x0C) //0x40010C08
+#define GPIOC_IDR_Addr (GPIOC_BASE + 0x0C) //0x40011008
+#define GPIOD_IDR_Addr (GPIOD_BASE + 0x0C) //0x40011408
+#define GPIOE_IDR_Addr (GPIOE_BASE + 0x0C) //0x40011808
+#define GPIOF_IDR_Addr (GPIOF_BASE + 0x0C) //0x40011A08
+#define GPIOG_IDR_Addr (GPIOG_BASE + 0x0C) //0x40011E08
+
+/* 单独操作 GPIO 的某一个 IO 口，n(0,1,2...16),n 表示具体是哪一个 IO 口 */
+#define PAout(n)    BIT_ADDR(GPIOA_ODR_Addr,n) //输出
+#define PAin(n)     BIT_ADDR(GPIOA_IDR_Addr,n) //输入
+
+#define PBout(n)    BIT_ADDR(GPIOB_ODR_Addr,n) //输出
+#define PBin(n)     BIT_ADDR(GPIOB_IDR_Addr,n) //输入
+
+#define PCout(n)    BIT_ADDR(GPIOC_ODR_Addr,n) //输出
+#define PCin(n)     BIT_ADDR(GPIOC_IDR_Addr,n) //输入
+
+#define PDout(n)    BIT_ADDR(GPIOD_ODR_Addr,n) //输出
+#define PDin(n)     BIT_ADDR(GPIOD_IDR_Addr,n) //输入
+
+#define PEout(n)    BIT_ADDR(GPIOE_ODR_Addr,n) //输出
+#define PEin(n)     BIT_ADDR(GPIOE_IDR_Addr,n) //输入
+
+#define PFout(n)    BIT_ADDR(GPIOF_ODR_Addr,n) //输出
+#define PFin(n)     BIT_ADDR(GPIOF_IDR_Addr,n) //输入
+
+#define PGout(n)    BIT_ADDR(GPIOG_ODR_Addr,n) //输出
+#define PGin(n)     BIT_ADDR(GPIOG_IDR_Addr,n) //输入
+/* ==================================================================================================== */
+/* ==================================================================================================== */
 
 /**
  * @description: 延迟函数
