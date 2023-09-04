2f7572d8a5713a672cc47b9fd65423f6c2301a9e
Chong Liu <liuc12@miamioh.edu>
2023-09-04 21:44:12 +0800

    Update main.c

--- 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/main.c
+++ 01_StdPeriph_Lib/02_GPIO/00_Keil_Fwlib_GPIO_Led/User/main.c
@@ -33,25 +33,25 @@ void Delay(__IO uint32_t nCount) {
 int main(void)
 {
 	// 来到这里的时候，系统的时钟已经被配置成72M。
-	LED_GPIO_Config();		/* LED GPIO初始化：默认拉低，此时所有初始化的 LED 灯都会被点亮 */
+    LED_GPIO_Config();		/* LED GPIO初始化：默认拉低，此时所有初始化的 LED 灯都会被点亮 */
 
     GPIO_SetBits(LED_GPIO, LED_R_PIN | LED_G_PIN | LED_B_PIN);		/* 使引脚输出高电平，关闭 LED */
-	Delay(0xFFFFFF);												/* 延时一段时间 */
+    Delay(0xFFFFFF);												/* 延时一段时间 */
 
-	while (1) {
+    while (1) {
         LED_SWITCH(LED_G_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Green */
         Delay(0x1FFFFF);			/* 延时一段时间 */
         LED_SWITCH(LED_G_PIN, OFF)	/* 使引脚输出高电平，关闭 LED Green */
         Delay(0x1FFFFF);			/* 延时一段时间 */
 
 
-		LED_SWITCH(LED_B_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Blue */
+        LED_SWITCH(LED_B_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Blue */
         Delay(0x1FFFFF);			/* 延时一段时间 */
         LED_SWITCH(LED_B_PIN, OFF)	/* 使引脚输出高电平，关闭 LED Blue */
         Delay(0x1FFFFF);			/* 延时一段时间 */
 
 
-		LED_SWITCH(LED_R_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Red */
+        LED_SWITCH(LED_R_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Red */
         Delay(0x1FFFFF);			/* 延时一段时间 */
         LED_SWITCH(LED_R_PIN, OFF)	/* 使引脚输出高电平，关闭 LED Red */
         Delay(0x1FFFFF);			/* 延时一段时间 */
