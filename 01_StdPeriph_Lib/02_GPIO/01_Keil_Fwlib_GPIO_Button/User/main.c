d82fa6901d9b3bf6731ad1a1966862acbfd3dad6
Chong Liu <liuc12@miamioh.edu>
2023-09-04 10:51:50 +0800

    Update main.c

--- 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/main.c
+++ 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/main.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-02 17:29:59
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-04 10:50:53
+ * @LastEditTime : 2023-09-04 10:51:26
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -31,13 +31,13 @@ void Delay(__IO uint32_t nCount) {
 int main(void)
 {
 	/* 来到这里的时候，系统的时钟已经被配置成72M */
-	LED_GPIO_Config();		/* LED GPIO初始化：默认输出拉低，此时所有初始化的 LED 灯都会被点亮 */
+    LED_GPIO_Config();		/* LED GPIO初始化：默认输出拉低，此时所有初始化的 LED 灯都会被点亮 */
     KEY_GPIO_Config(KEY1_GPIO_CLK, KEY1_GPIO, KEY1_PIN);            /* 按键1 初始化 */
 
     GPIO_SetBits(LED_GPIO, LED_R_PIN | LED_G_PIN | LED_B_PIN);      /* 使引脚输出高电平，关闭 LED */
-	Delay(0xFFFFFF);        /* 延时一段时间 */
+    Delay(0xFFFFFF);        /* 延时一段时间 */
 
-	while (1) {
+    while (1) {
         if (KEY_Scan(KEY1_GPIO, KEY1_PIN) == KEY_ON){
             LED_SWITCH(LED_G_PIN, ON)	/* 使引脚输出低电平, 点亮 LED Green */
         }
