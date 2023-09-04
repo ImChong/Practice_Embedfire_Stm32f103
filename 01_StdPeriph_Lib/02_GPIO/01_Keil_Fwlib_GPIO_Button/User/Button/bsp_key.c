d7e5ad451d8c90f5c7379b9613c47a4cdbcfa7bf
Chong Liu <liuc12@miamioh.edu>
2023-09-04 09:27:43 +0800

    Update bsp_key.c

--- 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/Button/bsp_key.c
+++ 01_StdPeriph_Lib/02_GPIO/01_Keil_Fwlib_GPIO_Button/User/Button/bsp_key.c
@@ -3,12 +3,13 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-02 17:39:04
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-04 08:11:13
+ * @LastEditTime : 2023-09-04 09:27:17
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  : BSP - Board Support Package：按键相关函数源文件
  */
+
 #include "bsp_key.h"    /* 此头文件将添加 stm32f10x.h */
 
 /**
@@ -38,5 +39,8 @@ void KEY_GPIO_Config(uint32_t RCC_GPIO_Clock, GPIO_TypeDef *GPIOx, uint16_t GPIO
  */
 void KEY_Scan(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
     /* TODO */
+    if (GPIO_ReadInputDataBit(GPIOx, GPIO_Pin)) {
+
+    }
     return;
 }
