0e8fee61315707df1e2d28bcd231a8181f2d6dab
Chong Liu <liuc12@miamioh.edu>
2023-10-03 14:20:40 +0800

    update

---
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/main.c
@@ -0,0 +1,22 @@
+/*
+ * @FilePath     : \02_Keil_Fwlib_Template\User\main.c
+ * @Author       : Chong Liu
+ * @CreateDate   : 2023-09-01 20:07:07
+ * @LastEditors  : Chong Liu
+ * @LastEditTime : 2023-09-04 18:54:41
+ * =================================================================================
+ * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
+ * =================================================================================
+ * @Description  : 主函数文件
+ */
+#include "stm32f10x.h"
+
+/**
+ * @description: 主函数
+ * =================================================================================
+ * @return {*}
+ */
+int main(void)
+{
+	// 来到这里的时候，系统的时钟已经被配置成72M。
+}
