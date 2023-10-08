c0dbb84dff6c8983964af19c9d95ef38e0e453ff
Chong Liu <liuc12@miamioh.edu>
2023-10-08 12:14:09 +0800

    update

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:11:00
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 12:08:46
+ * @LastEditTime : 2023-10-08 12:14:05
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -16,7 +16,7 @@
  * =================================================================================
  * @return {void}
  */
-void HSE_SetSysClk(void) {
+void HSE_SetSysClk(uint32_t RCC_PLLMul_x) {
     ErrorStatus HSEStatus;
 
     /* RCC->CR |= ((uint32_t)RCC_CR_HSEON); */
@@ -37,7 +37,7 @@ void HSE_SetSysClk(void) {
         RCC_PCLK2Config(RCC_HCLK_Div1);                         /* APB2 clock = HCLK = 72 MHz */
 
         /* RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL9); */
-        RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);    /* PLLCLK = HSE (8 MHz) * 9 = 72 MHz */
+        RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_x);    /* PLLCLK = HSE (8 MHz) * 9 (修改为参数 RCC_PLLMul_x 实现自定义倍频) = 72 MHz */
         RCC_PLLCmd(ENABLE);                                     /* PLL 使能 */
     } else {
         /* 如果 HSE 启动失败，用户可以在这里添加处理错误的代码 */
--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:12:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 11:30:34
+ * @LastEditTime : 2023-10-08 12:12:19
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -14,6 +14,6 @@
 
 #include "stm32f10x.h"
 
-void HSE_SetSysClk(void);
+void HSE_SetSysClk(uint32_t RCC_PLLMul_x);
 
 #endif  /* __BSP_RCCCLKCFG_H */
