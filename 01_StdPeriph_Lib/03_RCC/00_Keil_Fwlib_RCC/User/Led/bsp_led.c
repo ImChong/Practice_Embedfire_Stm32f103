c00054db029282431f13f0d9af6d673e2c609b6c
Chong Liu <liuc12@miamioh.edu>
2023-10-08 10:08:23 +0800

    Update bsp_led.c

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/Led/bsp_led.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/Led/bsp_led.c
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \01_Keil_Fwlib_GPIO_Button\User\Led\bsp_led.c
+ * @FilePath     : \00_Keil_Fwlib_RCC\User\Led\bsp_led.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-02 17:29:59
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-09-04 22:38:46
+ * @LastEditTime : 2023-10-08 10:08:21
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -17,11 +17,11 @@
  * @return {*}
  */
 void LED_GPIO_Config(void) {
-    GPIO_InitTypeDef GPIO_InitStructure;                                /* GPIO 端口初始化结构体 */
-    RCC_APB2PeriphClockCmd(LED_GPIO_CLK, ENABLE);                       /* 时钟使能 */
+    GPIO_InitTypeDef GPIO_InitStructure;                                            /* GPIO 端口初始化结构体 */
+    RCC_APB2PeriphClockCmd(LED_GPIO_CLK, ENABLE);                                   /* APB2 GPIOB 时钟使能 */
 
     GPIO_InitStructure.GPIO_Pin = LED_R_PIN_MSK | LED_G_PIN_MSK | LED_B_PIN_MSK;    /* 选择要控制的GPIO 引脚*/
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;                    /* 设置引脚的输出类型为推挽输出*/
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;                   /* 设置输出速率为 50MHZ */
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;                                /* 设置引脚的输出类型为推挽输出*/
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;                               /* 设置输出速率为 50MHZ */
     GPIO_Init(LED_GPIO, &GPIO_InitStructure);
 }
