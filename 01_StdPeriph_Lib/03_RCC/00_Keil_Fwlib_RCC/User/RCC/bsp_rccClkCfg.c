4ffd9b57bc4cebe8a15b17309786334e1f233654
Chong Liu <liuc12@miamioh.edu>
2023-10-08 11:57:13 +0800

    Update bsp_rccClkCfg.c

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:11:00
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 11:55:21
+ * @LastEditTime : 2023-10-08 11:56:59
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -26,8 +26,8 @@ void HSE_SetSysClk(void) {
     HSEStatus = RCC_WaitForHSEStartUp();
     if (HSEStatus == SUCCESS) {
         /* FLASH->ACR */
-        FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-        FLASH_SetLatency(FLASH_Latency_2);
+        FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);   /* Enable Prefetch Buffer | 使能预取址缓冲区 */
+        FLASH_SetLatency(FLASH_Latency_2);                      /* 两个等待状态，当 48MHz < SYSCLK ≤ 72MHz | 国产GD32声称无需等待时间 */
 
         /* RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1; */
         RCC_HCLKConfig(RCC_SYSCLK_Div1);                        /* AHB clock = SYSCLK = 72 MHz */
