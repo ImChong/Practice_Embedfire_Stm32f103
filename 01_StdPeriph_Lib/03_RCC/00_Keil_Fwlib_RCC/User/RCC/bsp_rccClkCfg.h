acae946d8515c7e4c9589143d9111d17a6084d7e
Chong Liu <liuc12@miamioh.edu>
2023-10-08 10:10:41 +0800

    Update bsp_rccClkCfg.h

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
@@ -1,11 +1,14 @@
 /*
- * @FilePath     : \Practice_Embedfire_Stm32f103\01_StdPeriph_Lib\03_RCC\00_Keil_Fwlib_RCC\User\RCC\bsp_rccClkCfg.h
+ * @FilePath     : \00_Keil_Fwlib_RCC\User\RCC\bsp_rccClkCfg.h
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:12:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-06 23:13:45
+ * @LastEditTime : 2023-10-08 10:09:47
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  :
  */
+#ifndef __BSP_RCCCLKCFG_H
+
+#endif // __BSP_RCCCLKCFG_H
