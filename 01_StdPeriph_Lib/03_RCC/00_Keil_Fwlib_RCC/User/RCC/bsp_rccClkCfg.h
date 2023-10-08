2aa68b0da71e98f71382442814c367bf37c66b68
Chong Liu <liuc12@miamioh.edu>
2023-10-08 10:12:52 +0800

    update

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.h
@@ -3,12 +3,15 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:12:51
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 10:09:47
+ * @LastEditTime : 2023-10-08 10:12:43
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
  * @Description  :
  */
 #ifndef __BSP_RCCCLKCFG_H
+#define __BSP_RCCCLKCFG_H
 
-#endif // __BSP_RCCCLKCFG_H
+
+
+#endif  /* __BSP_RCCCLKCFG_H */
