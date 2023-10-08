fb569107097c83a35fd1ed115614cb6f279af494
Chong Liu <liuc12@miamioh.edu>
2023-10-08 11:45:30 +0800

    update

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:11:00
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-08 11:37:03
+ * @LastEditTime : 2023-10-08 11:45:20
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -19,11 +19,12 @@
 void HSE_SetSysClk(void) {
     ErrorStatus HSEStatus;
 
-    RCC_HSEConfig(RCC_HSE_ON);                  /* 使能 HSE - 外部高速时钟 */
+    RCC_HSEConfig(RCC_HSE_ON);                                  /* 使能 HSE - 外部高速时钟 */
 
     HSEStatus = RCC_WaitForHSEStartUp();
     if (HSEStatus == SUCCESS) {
         FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+        FLASH_SetLatency(FLASH_Latency_2);
     } else {
         /* 如果 HSE 启动失败，用户可以在这里添加处理错误的代码 */
     }
