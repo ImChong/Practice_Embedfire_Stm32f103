c8d77f443d1c96905f04a4f5e3b419ff918005ab
Chong Liu <liuc12@miamioh.edu>
2023-10-08 11:29:28 +0800

    Update bsp_rccClkCfg.c

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/RCC/bsp_rccClkCfg.c
@@ -3,7 +3,7 @@
  * @Author       : Chong Liu
  * @CreateDate   : 2023-10-06 23:11:00
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-07 12:30:40
+ * @LastEditTime : 2023-10-08 11:28:46
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -11,6 +11,11 @@
  */
 #include "bsp_rccClkCfg.h"
 
+/**
+ * @description: 配置系统时钟为 HSE - 外部高速时钟
+ * =================================================================================
+ * @return {void}
+ */
 void HSE_SetSysClk() {
-
+    RCC_HSEConfig(RCC_HSE_ON);      /* 使能 HSE - 外部高速时钟 */
 }
