8e859bbfe63370f028e563e6ec03ee29267a0601
Chong Liu <liuc12@miamioh.edu>
2023-10-08 19:37:43 +0800

    Update main.c

--- 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/main.c
+++ 01_StdPeriph_Lib/03_RCC/00_Keil_Fwlib_RCC/User/main.c
@@ -1,9 +1,9 @@
 /*
- * @FilePath     : \03_RCC\00_Keil_Fwlib_RCC\User\main.c
+ * @FilePath     : \00_Keil_Fwlib_RCC\User\main.c
  * @Author       : Chong Liu
  * @CreateDate   : 2023-09-01 20:07:07
  * @LastEditors  : Chong Liu
- * @LastEditTime : 2023-10-06 23:19:36
+ * @LastEditTime : 2023-10-08 19:37:41
  * =================================================================================
  * Copyright (c) 2023 by Chong Liu, All Rights Reserved.
  * =================================================================================
@@ -18,5 +18,5 @@
  */
 int main(void)
 {
-	/* 来到这里的时候，系统的时钟已经被配置成72M。*/
+	/*  来到这里的时候，系统的时钟已经被配置成72M。*/
 }
